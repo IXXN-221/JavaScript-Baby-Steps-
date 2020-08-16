78fb606edf79bdb110bbff9276f2990cec224761
Zephiree <1risingkingdom@gmail.com>
2020-08-16 22:42:14 +1200

    done!

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById('demo').innerHTML = "Its turned on";
+    document.getElementById('8Ball').src = "magic8ballon.png";
+  }
+
+function myFunction1() {
+  document.getElementById('demo').innerHTML = "Its turned off";
+  document.getElementById('8Ball').src = "magic8balloff.png";
+}  
