1e884d685d7f09d82132c4d345ae13dcf548ed42
Yui131s <68263209+Yui131s@users.noreply.github.com>
2020-09-22 01:38:16 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function shutteron () {
+    document. getElementById('myImage').src='camera_on.png';
+}
+
+function shutteroff () {
+    document. getElementById('myImage'). src='camera_off.png';
+}
